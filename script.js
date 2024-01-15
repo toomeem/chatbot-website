ed248b29c24b2d6da1046e7ede857d01d094872d
Evan Toomey <64516913+toomeem@users.noreply.github.com>
2024-01-14 23:33:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-
-function hello_world_func(){
-  import hello_world from "main.py"
-  return hello_world();
+function hello_world_func(text){
+  const request = new XMLHttpRequest();
+  
+  request.addEventListener("readystatechange", () => {
+  	if(request.readyState === 4){
+  		console.log(request.responseText)
+  	}
+  });
+  console.log("Started")
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/1/");
+  request.send();
+  
 }
