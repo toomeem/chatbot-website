db33e40e352e81ad6a1ef74cb43cf34245cd3cc6
Vthegod2 <155935501+Vthegod2@users.noreply.github.com>
2024-01-17 12:52:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function hello_world_func(text){
   const request = new XMLHttpRequest();
-  //suppose to get user input from input box and display it
-  const input = document.getElementById("user-input");
-  const inputValue = input.value
   
   request.addEventListener("readystatechange", () => {
   	if(request.readyState === 4){
   		console.log(request.responseText)
   	}
   });
-  console.log(inputValue);
-  
+ 
   console.log("Started")
   request.open("GET", "https://actual-immune-cub.ngrok-free.app/website");
   request.send();
