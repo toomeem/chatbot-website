172852d810aa05c5f705f0bfedda51e9f6220b28
Vthegod2 <155935501+Vthegod2@users.noreply.github.com>
2024-01-17 10:40:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-Access-Control-Allow-Origin: *
-
-
 function hello_world_func(text){
-  //const request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
   //suppose to get user input from input box and display it
   const input = document.getElementById("user-input");
   const inputValue = input.value
   
-  //request.addEventListener("readystatechange", () => {
-  	//if(request.readyState === 4){
-  		//console.log(request.responseText)
-  	//}
+  request.addEventListener("readystatechange", () => {
+  	if(request.readyState === 4){
+  		console.log(request.responseText)
+  	}
   });
   console.log(inputValue);
   
-  //console.log("Started")
-  //request.open("GET", "https://actual-immune-cub.ngrok-free.app/website");
-  //request.send();
+  console.log("Started")
+  request.open("GET", "https://actual-immune-cub.ngrok-free.app/website");
+  request.send();
 
   
 }
